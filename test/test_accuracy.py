322575efded675cb19984637f34684e73dffbb1e
al3xber <71767468+al3xber@users.noreply.github.com>
2022-02-11 20:10:06 +0100

    Initial Version

---
+++ test/test_accuracy.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 10 16:25:30 2022
+
+@author: Alexander
+"""
+import sympy
+import numpy as np
+import double_slit as ds
+
+#Definition of all needed symbols
+x = sympy.Symbol('x', real=True)
+y = sympy.Symbol('y', real=True)
+x0 = sympy.Symbol('x_0', real=True)
+sx = sympy.Symbol('s_x', real=True)
+intensity = sympy.Symbol('I', real=True)
+phase = sympy.Symbol(r'\varphi', real=True)
+a2 = sympy.Symbol('a_2', real=True)
+a3 = sympy.Symbol('a_3', real=True)
+offset = sympy.Symbol('y_0', real=True)
+#-----------------------------------------------
+#Setting up the distribution of intensity
+
+dx = x - x0
+sdx = sx * dx
+    
+sinc_val = sympy.sinc(sdx)
+term1 = sympy.Rational(1, 2) * sinc_val**2
+term2_inner = a2 * sympy.cos(a3 * dx + phase)
+term2 = 1 + term2_inner 
+    
+y = intensity * term1 * term2 + offset #equals the final distribution
+#------------------------------------------------
+#Calculating derivatives
+
+dy_dx0 = sympy.diff(y, x0)
+dy_dI = sympy.diff(y, intensity)
+dy_dxs = sympy.diff(y, sx)
+dy_dy0 = sympy.diff(y, offset)
+dy_dphase = sympy.diff(y, phase)
+dy_da2 = sympy.diff(y, a2)
+dy_da3 = sympy.diff(y, a3)
+
+derivs = [dy_dx0, dy_dI, dy_dxs, dy_dy0, dy_dphase, dy_da2, dy_da3] #list of all derivatives
+
+
+
+def test_intensity_calc():
+    """
+    Checks the function double_slit, which calculates of the intensity distrubition 
+    Returns
+    -------
+    None.
+
+    """
+    z = np.linspace(-1, 1, num=10*6+1) #interval and number of points to check
+    
+    true_intensity = [y.evalf(subs={x: z_i,intensity: 1, sx:10, x0:0, offset:0, a2:2, a3:2, phase:1.5 * np.pi}) for z_i in z]
+                     #the function evalf takes a sympy expression and allows to insert values.
+                     
+    calc_intensity = ds.double_slit(z, intensity=1, sx=10, x0=0, offset=0, a2=2, a3=2, phase=1.5 * np.pi)
+
+    assert np.max(np.abs(true_intensity-calc_intensity))<10**(-15)
+
+
+def test_derivative_calc():
+    """
+    Checks the function calc_derivatives out of derivatives.py, which calculates of the derivatives 
+    Returns
+    -------
+    None.
+
+    """
+    z = np.linspace(-1, 1, num=10*6+1) #interval and number of points to check
+    
+    exact_derivs=[]
+    for i in range(len(derivs)):
+        exact_derivs.append([derivs[i].evalf(subs={x: z[j],intensity: 1,sx:10, x0:0.1, offset:0, a2:2, a3:2, phase:1.5 * np.pi}) for j in range(len(z))])
+    
+    calc_derivs=ds.calc_derivatives(z, intensity=1, sx=10, x0=0.1, offset=0, a2=2, a3=2, phase=1.5 * np.pi)
+    
+    for i in range(len(derivs)):
+        assert np.max(np.abs(exact_derivs[i]-calc_derivs[i]))<10**(-13)
---
+++ test/test_first.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 15:37:20 2022
+
+@author: Alexander
+"""
+import numpy as np
+import double_slit
+import sympy
+from scipy.special import sinc
+
+def test_signature():
+    """
+    Check if the functions matches the expected signature
+    Returns
+    -------
+    None.
+
+    """
+    double_slit.double_slit(0, intensity=1, sx=10, x0=0, offset=0, a2=2, a3=2, phase=1.5 * np.pi)
+    
+def test_scipy_and_sympy_sinc():
+    """
+    Check the relation between scipy.special and sympy sinc 
+    Further check scipy.special.sinc(x)=sin(x*pi)/(x*pi)
+    While         sympy.sinc(x)=sin(x)/x 
+    and check if  sympy.sinc(0)==scipy.special.sinc(0)== 1
+    -------
+    None.
+
+    """
+    x = sympy.Symbol('x', real=True)
+    y = sympy.sinc(x)
+    assert np.abs(y.evalf(subs={x:3})-sinc(3/np.pi))<10**(-17)
+    assert sinc(1/2)==2/(np.pi)
+    assert y.evalf(subs={x:0})==1 and sinc(0)==1
+    
+def test_offset():
+    """
+    Check if the offset parameter behaves as expected
+    The offset parameter only changes the height of the distribution
+    We first check that property
+    The next check is to ensure that the derivative is equal to 1
+    -------
+    None.
+
+    """
+    z = np.linspace(-1, 1, num=10*60+1)     
+    
+    assert np.all(double_slit.double_slit(z, intensity=1, sx=10, x0=0, offset=1, a2=2, a3=2, phase=1.5 * np.pi)  == \
+           double_slit.double_slit(z, intensity=1, sx=10, x0=0, offset=0, a2=2, a3=2, phase=1.5 * np.pi) + 1)
+           
+    assert np.all(double_slit.calc_derivatives(z, intensity=1, sx=10, x0=0.1, offset=0, a2=2, a3=2, phase=1.5 * np.pi)[3] == np.ones_like(z))
+
+
